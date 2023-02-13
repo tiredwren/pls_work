c8e3d69d3c3f75eccd2be52c0b305134aa3a9d9a
tiredwren <120143967+tiredwren@users.noreply.github.com>
2023-02-12 21:50:01 -0800

    full search?

---
+++ app/src/main/java/com/example/allhands/KissingCatsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.allhands
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KissingCatsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actiity_kissing_cats)
+    }
+}
--- app/src/main/java/com/example/allhands/MainActivity.kt
+++ app/src/main/java/com/example/allhands/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
@@ -12,7 +13,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemsAdapter.ClickListener {
 
     val itemNameImage = arrayOf(
             ItemsModal("converse","me when i grow up",R.drawable.converse),
@@ -33,11 +34,51 @@ class MainActivity : AppCompatActivity() {
         for(items in itemNameImage){
             itemModalList.add(items)
         }
-        itemsAdapter = ItemsAdapter();
+        itemsAdapter = ItemsAdapter(this);
         itemsAdapter!!.setData(itemModalList)
 
         this.findViewById<RecyclerView>(R.id.recyclerView).layoutManager = LinearLayoutManager(this);
         findViewById<RecyclerView>(R.id.recyclerView).setHasFixedSize(true)
         findViewById<RecyclerView>(R.id.recyclerView).adapter = itemsAdapter;
+    }
+
+    override fun ClickedItem(itemsModal: ItemsModal) {
+        Log.e("TAG",itemsModal.name)
+
+        when(itemsModal.name){
+            "converse" ->
+                startActivity(Intent(this@MainActivity, ConverseActivity::class.java))
+            "kissing_cats" ->
+                startActivity(Intent(this@MainActivity, KissingCatsActivity::class.java))
+
+            else -> {
+                Toast.makeText(this,"no action yet", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val menuItem = menu!!.findItem(R.id.searchView)
+
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.maxWidth = Int.MAX_VALUE
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(filterString: String?): Boolean {
+                itemsAdapter!!.filter.filter(filterString)
+                return true
+            }
+
+            override fun onQueryTextChange(filterString: String?): Boolean {
+
+                itemsAdapter!!.filter.filter(filterString)
+                return true
+            }
+
+        })
+        return true
     }
 }
