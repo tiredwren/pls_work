077eba0b7081794c6e4e8415ec762b1ba64feafc
Sneha M <moudgalya.sneha@gmail.com>
2023-02-22 22:42:16 -0800

    button onclicklistener + home launch?

--- app/src/main/java/com/example/allhands/HomeActivity.kt
+++ app/src/main/java/com/example/allhands/HomeActivity.kt
@@ -1,11 +1,22 @@
 package com.example.allhands
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val onlineButton = findViewById<Button>(R.id.online)
+        onlineButton.setOnClickListener {
+            val Intent = Intent(this, MainActivity::class.java)
+            startActivity(Intent)
+
+        /* dont forget to link "in person" button to MapActivity once you have it!*/
+        }
     }
+
 }
